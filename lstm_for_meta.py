:bug: bug: make s tensors of output module layers stand in gradient path
Split s tensor, put splitting result in optimizer ins, concat it back. If split s and put in in optimizer ins without
following concatenation and passing it to activation function and then to next layers sigma will be None
because s does not stand in loss computation path.

lstm_for_meta.py
@@ -278,22 +278,26 @@ class Lstm(Model):
             else:
                 concat_dim = 0
             num_split = len(rnn_outputs)
+            o = rnn_outputs
             rnn_outputs = tf.concat(rnn_outputs, concat_dim, name='concatenated_rnn_outputs')
             hs = rnn_outputs
             for layer_idx, (matr, bias) in enumerate(zip(output_matrices, output_biases)):
-                # print('hs.shape:', hs.get_shape().as_list())
-                s = custom_matmul(
-                        hs, matr)
-                optimizer_ins['output_layer_%s' % layer_idx] = dict(
-                    o=tf.split(hs, num_split, axis=concat_dim),
-                    s=tf.split(s, num_split, axis=concat_dim)
-                )
-                hs = custom_add(
-                    s,
-                    bias,
-                    name='res_of_%s_output_layer' % layer_idx)
-                if layer_idx < self._num_output_layers - 1:
-                    hs = tf.nn.relu(hs)
+                with tf.name_scope('layer_%s' % layer_idx):
+                    # print('hs.shape:', hs.get_shape().as_list())
+                    s = custom_matmul(
+                            hs, matr, name='first_s')
+                    s = tf.split(s, num_split, axis=concat_dim, name='split_s_for_optimizer')
+                    optimizer_ins['output_layer_%s' % layer_idx] = dict(
+                        o=o,
+                        s=s
+                    )
+                    s = tf.concat(s, concat_dim, name='united_s')
+                    hs = custom_add(
+                        s,
+                        bias,
+                        name='res_of_%s_output_layer' % layer_idx)
+                    if layer_idx < self._num_output_layers - 1:
+                        hs = tf.nn.relu(hs)
         return hs, optimizer_ins
 
     def _compute_lstm_matrix_parameters(self, idx):

meta.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from useful_functions import (construct, get_keys_from_nested, get_obj_elem_by_path, device_name_scope,
                               write_elem_in_obj_by_path, stop_gradient_in_nested, compose_save_list, average_gradients,
-                              retrieve_from_inner_dicts, distribute_into_inner_dicts)
+                              retrieve_from_inner_dicts, distribute_into_inner_dicts, print_optimizer_ins)
 
 
 class Meta(object):
@@ -246,12 +246,15 @@ class Meta(object):
     def _eval_pupil_gradients_for_optimizer_inference(self):
         loss, optimizer_ins, storage_save_ops = self._pupil.loss_and_opt_ins_for_inference()
         s_vectors, map_ = retrieve_from_inner_dicts(optimizer_ins, 's')
+        print('(Meta._eval_pupil_gradients_for_optimizer_inference)map_:', map_)
         sigma_vectors = tf.gradients(loss, s_vectors)
         optimizer_ins = distribute_into_inner_dicts(optimizer_ins, 'sigma', sigma_vectors, map_)
         return optimizer_ins, storage_save_ops, loss
 
     @staticmethod
     def _empty_core(optimizer_ins, learning_rate=4.):
+        print('(Meta._empty_core)optimizer_ins:')
+        print_optimizer_ins(optimizer_ins)
         for v in optimizer_ins.values():
             if isinstance(v['o'], list):
                 v['phi'] = tf.add_n(v['o']) / len(v['o']) * learning_rate**.5