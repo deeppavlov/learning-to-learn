5ae0e04132c1a5b8c467282e978c3f1aeac9c782
Anton Peganov <peganov@phystech.edu>
2018-06-16 18:00:52 +0300

    :star: new: add test experiments

--- learning_to_learn/experiments/catexp.sh
+++ learning_to_learn/experiments/catexp.sh
@@ -18,8 +18,6 @@ catrec() {
       do
         #echo "$f"
         name="${f%.*}"
-        #cp "$f" "$name"_pdf_descr.txt
-        #echo -e "\n$new_path" >> "$name"_pdf_descr.txt
         for_joining_name="$new_path"/join_"$counter".pdf
         ((counter++))
         descr_size=$(stat -c%s $f)
@@ -32,7 +30,7 @@ catrec() {
         convert -size 900x60 xc:white -pointsize 36 -fill red -draw "text 5,30 \"$new_path  $descr\" " "$name"_descr.png
         convert "$name"_descr.png "$name"_descr.pdf
         pdfjam "$name".pdf "$name"_descr.pdf --nup 1x2 --outfile "$for_joining_name"
-        #rm "$name"*_descr*.png "$name"*_descr*.pdf "$name"*_descr*.txt
+        rm "$name"*_descr*.png "$name"*_descr*.pdf
       done
     fi
   done
