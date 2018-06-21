:bug: bug: fix bug in log and sign preprocessing

learning_to_learn/optimizers/l2l.py
@@ -86,7 +86,7 @@ class L2L(Meta):
         else:
             input_dim = self._num_lstm_nodes[idx - 1] + self._num_lstm_nodes[idx]
         output_dim = 4 * self._num_lstm_nodes[idx]
-        stddev = self._optimizer_init_parameter * np.sqrt(1. / input_dim)
+        stddev = self._optimizer_init_parameter * np.sqrt(1. / (input_dim + output_dim))
         return input_dim, output_dim, stddev
 
     def _create_optimizer_trainable_vars(self):

learning_to_learn/tensors.py
@@ -130,6 +130,6 @@ def log_and_sign(inp, p):
     less_second = np.exp(p) * inp
     greater = tf.stack([greater_first, greater_second], axis=-1)
     less = tf.stack([less_first, less_second], axis=-1)
-    mask = tf.expand_dims(tf.to_float(inp > edge), axis=-1)
+    mask = tf.expand_dims(tf.to_float(tf.abs(inp) > edge), axis=-1)
     res = mask * greater + (1. - mask) * less
     return res