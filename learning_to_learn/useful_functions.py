a6ec4aa9ef51689c164dfc60c6a2051e7ebd9a98
Anton Peganov <peganov@phystech.edu>
2019-01-25 16:45:19 +0300

    debug

--- learning_to_learn/useful_functions.py
+++ learning_to_learn/useful_functions.py
@@ -1538,6 +1538,7 @@ def extract_metrics(line, metric_names):
 
 def get_combs_and_num_exps_pupil(eval_file, order):
     tested_combs = list()
+    print("(useful_functions.get_combs_and_num_exps_pupil)eval_file:", eval_file)
     if os.path.exists(eval_file):
         with open(eval_file, 'r') as f:
             lines = f.read().split('\n')
